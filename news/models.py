2b9bd29fc480cd4a0aec65d2f0dedc2898455fd2
UNIVERS10 <joelattitso@gmail.com>
2025-02-28 13:41:29 +0000

    Page news ok, page detail news en cours

--- news/models.py
+++ news/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-
 import os
 
 def news_directory_path(instance, filename):
-    
     return os.path.join('news', 'media', 'profiles', filename)
 
 class News(models.Model):
@@ -17,9 +15,16 @@ class News(models.Model):
     is_featured = models.BooleanField(default=False, verbose_name="Actualité en vedette")
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Créateur")
     external_link = models.URLField(blank=True, null=True, verbose_name="Lien externe")
+    resume = models.CharField(max_length=500, blank=True, null=True, verbose_name="Résumé")
 
     def comment_count(self):
         return self.comments.count()
+
+    def save(self, *args, **kwargs):
+        # Auto-remplissage du résumé si non fourni : on prend les 200 premiers caractères du contenu
+        if not self.resume and self.content:
+            self.resume = self.content[:200] + ("..." if len(self.content) > 200 else "")
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Actualité"
@@ -28,7 +33,7 @@ class News(models.Model):
 
     def __str__(self):
         return self.headline
-    
+
 class NewsComment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='news_comments', verbose_name="Commentateur")
     news = models.ForeignKey(News, on_delete=models.CASCADE, related_name="comments", verbose_name="Actualité")
